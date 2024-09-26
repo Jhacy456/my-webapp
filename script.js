0cb87721026ca6813f52e3cd16811245dd10c558
Jhacy456 <jakpesad20@gmail.com>
2024-09-26 12:24:34 +0000

    started creating an web app

---
+++ script.js
@@ -0,0 +1,116 @@
+const searchMeal = async (e) => {
+    e.preventDefault();
+
+    // select elements
+    const input = document.querySelector('.input')
+    const title = document.querySelector('.title')
+    const info = document.querySelector('.info')
+    const img = document.querySelector('.img')
+    const ingredientsOutput = document.querySelector('.ingredients')
+
+    const showMealInfo = (meal) => {
+        // console.log(meal);
+        const { strMeal, strMealThumb, strInstructions } = meal;
+        title.textContent = strMeal
+        img.style.backgroundImage = `url(${strMealThumb})`
+        info.textContent = strInstructions
+
+        const ingredients = []
+
+        for (let i = 1; i <= 20; i++) {
+            if (meal[`strIngredients${i}`]) {
+                ingredients.push(`${meal[`strIngredients${i}`]} - ${meal[`strMeasures${i}`]}`)
+            } else {
+                break;
+            }
+        }
+
+        const html = `
+      <span>${ingredients.map((ing) => `<li class="ing">${ing}</li>`).join("")}</span>
+      
+      `
+        ingredientsOutput.innerHTML = html
+
+    };
+
+
+
+
+
+    const showAlert = () => {
+        alert('meal not found:(')
+    };
+
+
+
+    // fecth data
+    const fetchMealData = async (val) => {
+        const response = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${val}`
+        );
+
+        const { meals } = await response.json()
+        return meals;
+    };
+
+    // get the user value
+    const val = input.value.trim()
+
+    if (val) {
+        const meals = await fetchMealData(val)
+
+        if (!meals) {
+            showAlert()
+            return;
+        }
+        meals.forEach(showMealInfo)
+
+
+    } else {
+        alert("please try searching for meal :)")
+    }
+
+}
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit", searchMeal);
+
+const magnifier = document.querySelector("magnifier");
+magnifier.addEventListener("click", searchMeal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
